bce42f49b72726d3a2f420a41e21d44a1ccf22b2
Andrew <huandrew321@gmail.com>
2026-05-07 23:53:09 -0400

    Revert: inline Firebase config, remove Actions workflow

---
+++ config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAe_2xEx6uCEPZr4MdOIBw4gCSJKxC1wi4",
+  authDomain: "loss-tracker-7bd9a.firebaseapp.com",
+  projectId: "loss-tracker-7bd9a",
+  storageBucket: "loss-tracker-7bd9a.firebasestorage.app",
+  messagingSenderId: "147406962467",
+  appId: "1:147406962467:web:ab3fba8fa191a8e9ac5696"
+};
