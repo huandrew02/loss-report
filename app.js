492f3832a69ba5fcfcbc80bf6ce5cc4dcc8522b0
Andrew <huandrew321@gmail.com>
2026-05-09 23:44:58 -0400

    Remove fallback, API key only from secret now

--- app.js
+++ app.js
@@ -27,15 +27,7 @@ function defaultData() {
 }
 
 // ---- Firebase ----
-const FB_CONFIG = typeof FIREBASE_CONFIG !== 'undefined' ? FIREBASE_CONFIG : {
-  apiKey: "AIzaSyAe_2xEx6uCEPZr4MdOIBw4gCSJKxC1wi4",
-  authDomain: "loss-tracker-7bd9a.firebaseapp.com",
-  projectId: "loss-tracker-7bd9a",
-  storageBucket: "loss-tracker-7bd9a.firebasestorage.app",
-  messagingSenderId: "147406962467",
-  appId: "1:147406962467:web:ab3fba8fa191a8e9ac5696"
-};
-firebase.initializeApp(FB_CONFIG);
+firebase.initializeApp(FIREBASE_CONFIG);
 const DB = firebase.firestore();
 const AUTH = firebase.auth();
 
