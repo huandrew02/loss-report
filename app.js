3ad1cb3ea0d57abf43d8ba1e9b0594df24c39213
Andrew <huandrew321@gmail.com>
2026-05-09 23:40:26 -0400

    Add fallback config so site works even before workflow runs

--- app.js
+++ app.js
@@ -27,7 +27,15 @@ function defaultData() {
 }
 
 // ---- Firebase ----
-firebase.initializeApp(FIREBASE_CONFIG);
+const FB_CONFIG = typeof FIREBASE_CONFIG !== 'undefined' ? FIREBASE_CONFIG : {
+  apiKey: "AIzaSyAe_2xEx6uCEPZr4MdOIBw4gCSJKxC1wi4",
+  authDomain: "loss-tracker-7bd9a.firebaseapp.com",
+  projectId: "loss-tracker-7bd9a",
+  storageBucket: "loss-tracker-7bd9a.firebasestorage.app",
+  messagingSenderId: "147406962467",
+  appId: "1:147406962467:web:ab3fba8fa191a8e9ac5696"
+};
+firebase.initializeApp(FB_CONFIG);
 const DB = firebase.firestore();
 const AUTH = firebase.auth();
 
